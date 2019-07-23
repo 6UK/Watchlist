cefd27c09449ba89220906413f23052c0d96fbd9
JOFLIX <joflixooko@outlook.com>
2019-07-23 11:27:57 +0300

    Second commit to generate url for api registration

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 
+
+
 # Views
 @app.route('/')
 def index():
@@ -9,5 +11,15 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello there, it is time to launch the killer attack!'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+
+    return render_template('movie.html')
+
